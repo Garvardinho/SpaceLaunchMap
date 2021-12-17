df72d9c62fe81cb3643f31d55e3b59d4a00205cc
Gavriil Petrov <garvardinho@mail.ru>
2021-12-17 23:56:45 +0300

    Launchpad info view updated

--- app/src/main/java/com/spacelaunchmapandroid/spacelaunchmap/flow/map/MapFragment.kt
+++ app/src/main/java/com/spacelaunchmapandroid/spacelaunchmap/flow/map/MapFragment.kt
@@ -2,6 +2,7 @@ package com.spacelaunchmapandroid.spacelaunchmap.flow.map
 
 import android.graphics.Paint
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -79,6 +80,9 @@ class MapFragment : Fragment(), SLMapView {
                 ) as TextView
                 launchpadTitleList.paintFlags = Paint.UNDERLINE_TEXT_FLAG
                 launchpadTitleList.text = title
+                launchpadTitleList.width = 256
+                launchpadTitleList.height = 128
+                launchpadTitleList.gravity = Gravity.CENTER
                 launchpadTitleListLayout.addView(launchpadTitleList)
             }
 
