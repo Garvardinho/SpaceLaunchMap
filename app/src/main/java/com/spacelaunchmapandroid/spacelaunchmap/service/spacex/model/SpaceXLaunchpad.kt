0e11365ecf45941fc7ef4581c802e8019f200895
Gavriil Petrov <garvardinho@mail.ru>
2021-12-20 00:40:22 +0300

    Launch screen made

--- app/src/main/java/com/spacelaunchmapandroid/spacelaunchmap/service/spacex/model/SpaceXLaunchpad.kt
+++ app/src/main/java/com/spacelaunchmapandroid/spacelaunchmap/service/spacex/model/SpaceXLaunchpad.kt
@@ -1,6 +1,7 @@
 package com.spacelaunchmapandroid.spacelaunchmap.service.spacex.model
 
 data class SpaceXLaunchpad(
+    var id: String,
     var name: String,
     var locality: String,
     var region: String,
